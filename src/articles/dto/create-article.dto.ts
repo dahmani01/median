6122d32ba21bfb38c795facbce119063003784ce
Ahmed <adahmani010@gmail.com>
2023-06-21 15:46:07 +0100

    Added Input validation using Pipes

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,14 +1,22 @@
 import { Body } from '@nestjs/common';
 import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateArticleDto {
-
+    @IsString()
+    @MinLength(5)
     @ApiProperty()
-    title : string; 
+    title : string;
+    @IsString()
+    @IsOptional()
+    @MaxLength(500)
     @ApiProperty({required : false})
     description? : string
+    @IsString()
     @ApiProperty()
     body : string ;
+    @IsBoolean()
+    @IsOptional()
     @ApiProperty({required : false, default:false})
     published? : boolean = false ;  
 }
